ae3ac69813ade064f9831bf1e4e0be263afb12fd
T-Abt <abt.flo@web.de>
2017-03-01 14:54:39 +0100

    ScannerControllerService send-Mehtods implemented

--- cocome-jadex-services/src/main/java/fypa2c/cocome/tradingsystem/cashdeskline/components/scannerController/ScannerControllerService.java
+++ cocome-jadex-services/src/main/java/fypa2c/cocome/tradingsystem/cashdeskline/components/scannerController/ScannerControllerService.java
@@ -1,18 +1,19 @@
 package fypa2c.cocome.tradingsystem.cashdeskline.components.scannerController;
 
+import fypa2c.cocome.tradingsystem.cashdeskline.components.EventService;
+import fypa2c.cocome.tradingsystem.cashdeskline.events.ProductBarcodeScannedEvent;
 import jadex.bridge.service.annotation.Service;
 import jadex.bridge.service.annotation.ServiceComponent;
 import jadex.commons.future.IFuture;
 
 @Service
-public class ScannerControllerService implements IScannerControllerService
+public class ScannerControllerService extends EventService implements IScannerControllerService
 {
 	@ServiceComponent
 	protected ScannerControllerAgent component;
 
 	@Override
 	public IFuture<Void> sendProductBarCodeScannedEvent(int barcode) {
-		// TODO Auto-generated method stub
-		return null;
+		return publishEvent(new ProductBarcodeScannedEvent(barcode));
 	}
 }
